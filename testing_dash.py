14414f551375d9d9a3e4f50696e5edad30af4817
Bhavya Muni <bhavyamuni@outlook.com>
2022-10-10 14:58:13 -0400

    testing plotly daash

--- testing_dash.py
+++ testing_dash.py
@@ -1,13 +1,49 @@
-import time
-import plotly.graph_objects as go
+import dash
+from dash.dependencies import Output, Input
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly
+import random
+import plotly.graph_objs as go
+from collections import deque
 
-data = [1,3,2,4,3,3,2,3]
+X = deque(maxlen = 20)
+X.append(1)
 
+Y = deque(maxlen = 20)
+Y.append(1)
 
-fig = go.FigureWidget()
-fig.add_scatter()
-fig
-for i in range(len(data)):
-    time.sleep(0.3)
-    fig.data[0].y = data[:i] 
+app = dash.Dash(__name__)
 
+app.layout = html.Div(
+	[
+		dcc.Graph(id = 'live-graph', animate = True),
+		dcc.Interval(
+			id = 'graph-update',
+			interval = 25,
+			n_intervals = 0
+		),
+	]
+)
+
+@app.callback(
+	Output('live-graph', 'figure'),
+	[ Input('graph-update', 'n_intervals') ]
+)
+
+def update_graph_scatter(n):
+	X.append(X[-1]+1)
+	Y.append(Y[-1]+Y[-1] * random.uniform(-0.1,0.1))
+
+	data = plotly.graph_objs.Scatter(
+			x=list(X),
+			y=list(Y),
+			name='Scatter',
+			mode= 'lines+markers'
+	)
+
+	return {'data': [data],
+			'layout' : go.Layout(xaxis=dict(range=[min(X),max(X)]),yaxis = dict(range = [min(Y),max(Y)]),)}
+
+if __name__ == '__main__':
+	app.run_server()
