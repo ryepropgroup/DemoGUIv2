ca27aa4a562f98101c11a333801617e38ab055f1
Daniel <ohmyzazu@gmail.com>
2023-01-14 13:24:31 -0500

    engine comp

--- engine_computer.py
+++ engine_computer.py
@@ -1,10 +1,10 @@
-import socket, time, serial 
+# import socket, serial, time
 
-HOST = "169.254.205.14"
-# The servers hostname or IP address
-PORT = 65432 # The port used by the server
+# HOST = "10.42.0.40"
+# # The servers hostname or IP address
+# PORT = 65432 # The port used by the server
 
-# arduino = serial.Serial(port='COM4', baudrate=115200, timeout=.1)
+# arduino = serial.Serial(port='/dev/ttyACM0', baudrate=115200, timeout=.1)
 
 # def write_read(x):
 #     arduino.write(bytes(x, 'utf-8'))
@@ -13,16 +13,16 @@ PORT = 65432 # The port used by the server
 #     return data
 
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect ((HOST, PORT))
-    data = s.recv(1024)
-    print(data.decode('utf-8'))
-    if data.decode('utf-8') == 'Welcome to Borealis Mission Control':
-        while True:
-            data = s.recv(1024)
-            print(data.decode('utf-8'))
-            if data == b"Open":
-                s.sendall(b'valves opened')
-            if data == b"quit":
-                s.close()
-                exit()
+# with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+#     s.connect ((HOST, PORT))
+#     data = s.recv(1024)
+#     print(data.decode('utf-8'))
+#     if data.decode('utf-8') == 'Welcome to Borealis Mission Control':
+#         while True:
+#             data = s.recv(1024)
+#             print(data.decode('utf-8'))
+#             if data == b"hello":
+#                 write_read("1")
+#             if data == b"quit":
+#                 s.close()
+#                 exit()
